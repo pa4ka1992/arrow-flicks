6825903f81276028a56ec7c446150cab5b18d9d3
Nikita Kisly <sour945@gmail.com>
2024-05-23 08:21:00 +0300

    refactor: update images with next image

--- apps/web/src/components/MovieCard/components/Poster/constants.ts
+++ apps/web/src/components/MovieCard/components/Poster/constants.ts
@@ -1,8 +1,6 @@
-import { AnchorProps } from '@mantine/core';
-
 import { MovieCardVariant } from '../../constants';
 
-export const PosterVariant: Record<`${MovieCardVariant}`, AnchorProps> = {
+export const PosterVariant = {
   [MovieCardVariant.LIST]: {
     maw: { base: 200, xs: 120 },
     h: { base: 300, xs: 170 },
@@ -10,7 +8,6 @@ export const PosterVariant: Record<`${MovieCardVariant}`, AnchorProps> = {
 
   [MovieCardVariant.PAGE]: {
     maw: { base: 230, xs: 250 },
-    miw: 0,
     h: { base: 330, xs: 350 },
   },
 };
--- apps/web/src/components/MovieCard/components/Poster/index.tsx
+++ apps/web/src/components/MovieCard/components/Poster/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import NextImage from 'next/image';
 import Link from 'next/link';
 import { Anchor, Image } from '@mantine/core';
 import { Movie } from 'app-types';
@@ -9,6 +10,7 @@ import { RoutePath } from 'routes';
 import config from 'config';
 
 import NoPoster from '../NoPoster';
+import { rgbDataURL } from './utils/get-placeholder.util';
 import { PosterVariant } from './constants';
 
 import classes from './index.module.css';
@@ -22,14 +24,31 @@ interface PosterProps {
 const Poster: FC<PosterProps> = ({ movieId, path, variant }) => {
   const posterProps = PosterVariant[variant];
 
-  const poster = path ? <Image alt="movie poster" height="100%" src={`${config.TMDB_URL}/${path}`} /> : <NoPoster />;
+  const poster = path ? (
+    <Image
+      component={NextImage}
+      alt="movie poster"
+      fill
+      sizes="100vw"
+      src={`${config.TMDB_URL}/${path}`}
+      quality={80}
+      placeholder="blur"
+      blurDataURL={rgbDataURL(152, 84, 246)}
+    />
+  ) : (
+    <NoPoster />
+  );
 
   return (
     <Anchor
+      bg="primary.6"
       className={classes.link}
       component={Link}
       display="block"
       href={{ pathname: RoutePath.Movie, query: { id: movieId } }}
+      mih={{ base: posterProps.h.base, xs: 'auto' }}
+      miw={{ base: posterProps.maw.base, xs: 'auto' }}
+      pos="relative"
       underline="never"
       {...posterProps}
     >
